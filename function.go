1804664a23d811657d37b889fe228a10ab97b421
zhiqiangwang <zhiqiangwang@aliyun.com>
2023-08-14 13:39:14 +0800

    feat: function && decode

---
+++ function.go
@@ -0,0 +1,33 @@
+package gdi
+
+var (
+	di = New()
+)
+
+func Register(provider IServiceProvider) IContainer {
+	return di.Register(provider)
+}
+func Handler(handlers ...HandlerFunc) IContainer {
+	return di.Handler(handlers...)
+}
+func Set(id string, value any) {
+	di.Set(id, value)
+}
+func Get(id string) any {
+	return di.Get(id)
+}
+func Exists(id string) bool {
+	return di.Exists(id)
+}
+func Unset(id string) {
+	di.Unset(id)
+}
+func Raw(id string) (any, error) {
+	return di.Raw(id)
+}
+func Values() map[string]any {
+	return di.Values()
+}
+func Decode(val any) error {
+	return di.Decode(val)
+}
